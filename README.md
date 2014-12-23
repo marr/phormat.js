aff998eda59e07542769889a77171a2b7d711b1a
Dave Marr <dave.marr@gmail.com>
2014-12-22 22:27:04 -0800

    Transform a ListBucketResult from AWS to JSON.

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 phormat
 -------
 
-Usage - coming soon
+# install #
+`npm install phormat`
+
+# config #
+`s3config.json` should include the following:
+```
+{
+	"accessKeyId": "YOUR_S3_ACCESS_KEY_HERE",
+	"secretAccessKey": "YOUR_S3_SECRET_HERE"
+}
+```
+
+# usage #
+```
+var phormat = require('phormat')
+
+// use the bucketlist to retrieve a s3 object from aws
+phormat
+    .bucketlist()
+    .createReadStream()
+    .pipe(process.stdout)
+```
--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-module.exports = {
-    pathname: require('./lib/pathname'),
-    bucketlist: require('./lib/bucketlist')
+var bucketlist = require('./lib/bucketlist');
+var through2 = require('through2');
+var XmlStream = require('xml-stream');
+
+function processBucket(xmlBuffer) {
+    var data = xmlBuffer.toString();
+    return data
+}
+
+module.exports = function(callback) {
+
+    var allData = [];
+    var xmlStream = new bucketlist().createReadStream();
+    xmlStream = new XmlStream(xmlStream);
+    xmlStream.on('updateElement: Contents', function(data) {
+        data = processBucket(data);
+        allData.push(data);
+    });
+    xmlStream.on('end', function() {
+        callback(allData);
+    });
+    xmlStream.on('error', function(message) {
+        console.error(err);
+    });
 };
